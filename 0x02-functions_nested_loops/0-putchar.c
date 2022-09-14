f6a42821e4c843d8e834d2078c9582b3a633a8b6
Linah-almasi <110978230+Linah-almasi@users.noreply.github.com>
2022-09-14 08:13:08 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - prints Putchar
  *
@@ -6,18 +7,19 @@
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-_putchar('_');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	return (0);
 }
 
