c64a0b8b0945479911fa2e1f0cf5035e57217bd0
Linah-almasi <almasilinah@gmail.com>
2022-09-15 08:41:06 -0700

    qf

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 without using standard functions
- *
- *
- * Return: void
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
-	int c = 48;
+	int i;
 
-	while (c < 58)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(c);
-		c++;
+		_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }
